c6af72d2c18c3cf978e247ac9aa82d03c42c43c0
Hyunjin Park <132244618+hyun-Jin-Park-4808@users.noreply.github.com>
2025-01-19 22:14:51 +0900

    [level 2] Title: 기능개발, Time: 3.26 ms, Memory: 87.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -3,31 +3,28 @@ import java.util.stream.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        Queue<Integer> que = new LinkedList<>();
-        for(int i = 0 ; i < progresses.length ; i++) {
-            que.add(i); // 큐에 인덱스 하나씩 추가
+        Queue<Integer> answer = new ArrayDeque<>();
+        int n = progresses.length;
+        int[] daysLeft = new int[n];
+        
+        for(int i = 0; i < n; i++) { // 각 작업의 배포 가능일 계산 
+            daysLeft[i] = (int) Math.ceil((100.0 - progresses[i]) / speeds[i]);
         }
         
-        List<Integer> result = new ArrayList<>();
-        int days = 0; // 현재까지 걸린 일수
-        int count = 0; // 동시 배포할 작업 수
-
-        while(!que.isEmpty()) {
-            int index = que.poll(); // 인덱스 하나 꺼내오기 
-            int expiration // 현재 인덱스의 작업 완료까지 걸리는 일수 
-                = (int) Math.ceil((double) (100 - progresses[index]) / speeds[index]); 
-            
-            if(expiration > days) { // 현재 인덱스의 작업 완료까지 걸리는 일수가 현재까지 걸린 일수보다 크면 
-                if(days != 0) { // 현재까지 걸린 일수가 아예 없는 경우가 아니면
-                    result.add(count); // 결과 리스트에 누적된 동시 배포 작업 수 더해주고  
-                    count = 0; // 동시 배포 작업 수 0으로 초기화 
-                }
-                days = expiration; // 현재까지 걸린 일수 업데이트 
+        int count = 0; // 배포될 작업의 수
+        int maxDay = daysLeft[0]; // 현재 배포될 작업 중 가장 늦게 배포될 작업의 배포 가능일 
+        
+        for(int i = 0; i < n; i++) {
+            if(daysLeft[i] <= maxDay) { // 현재 작업의 배포 가능일이 앞선 작업의 배포 가능일보다 빠르면 
+                count++; // 배포 아직 못함. 
             }
-            count++; // 동시 배포할 작업 수 증가
+            else { // 더 느리면 
+                answer.add(count); // 앞선 작업 배포 
+                count = 1; // 배포될 작업의 수 초기화 
+                maxDay = daysLeft[i]; // 다음 기능의 배포일로 maxDay 초기화 
+            }
         }
-        
-        result.add(count); // 남은 배포할 작업수 추가
-        return result.stream().mapToInt(Integer::intValue).toArray();
+        answer.add(count); // 마지막으로 카운트된 작업들 함께 배포 
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
