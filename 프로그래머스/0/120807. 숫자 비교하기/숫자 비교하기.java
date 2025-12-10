5e5222772f957268e4360ecbaa1e83058e6fe2f0
Hyunjin Park <132244618+hyun-Jin-Park-4808@users.noreply.github.com>
2025-12-10 20:20:57 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        if(num1 >= 0 && num1 <= 10000 && num2 >= 0 && num2 <= 10000) {
+            return num1 == num2 ? 1 : -1;
+        }
+        return -1;
+    }
+}
