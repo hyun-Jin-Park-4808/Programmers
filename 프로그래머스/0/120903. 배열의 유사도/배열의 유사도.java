dbb60bad7beaaf5a661a8448205b98686bcf92fd
Hyunjin Park <132244618+hyun-Jin-Park-4808@users.noreply.github.com>
2025-12-13 21:05:55 +0900

    [level 0] Title: 배열의 유사도, Time: 0.08 ms, Memory: 68.2 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
@@ -2,16 +2,9 @@ import java.util.*;
 
 class Solution {
     public int solution(String[] s1, String[] s2) {
-        Set<String> set1 = new HashSet<>();
-        Set<String> set2 = new HashSet<>();
+        Set<String> set1 = new HashSet<>(List.of(s1));
+        Set<String> set2 = new HashSet<>(List.of(s2));
         
-        for(String n : s1) {
-            set1.add(n);
-        }
-        
-        for(String n : s2) {
-            set2.add(n);
-        }
         set1.retainAll(set2);
         return set1.size();
     }
