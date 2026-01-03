12250c1b091308a9740c97e9a3a77fb6994ca118
Hyunjin Park <132244618+hyun-Jin-Park-4808@users.noreply.github.com>
2026-01-03 22:42:54 +0900

    [level 0] Title: 자릿수 더하기, Time: 3.08 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        return String.valueOf(n)
+            .chars()
+            .map(c -> c - '0')
+            .sum();
+    }
+}
