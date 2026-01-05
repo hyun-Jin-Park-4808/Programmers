d900f1daea196638d9d125cbd3acd8fe169f20e4
Hyunjin Park <132244618+hyun-Jin-Park-4808@users.noreply.github.com>
2026-01-05 22:03:29 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.02 ms, Memory: 85.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        char[] chars = my_string.toCharArray();
+        chars[num1] = my_string.charAt(num2);
+        chars[num2] = my_string.charAt(num1);
+        return String.valueOf(chars);
+    }
+}
