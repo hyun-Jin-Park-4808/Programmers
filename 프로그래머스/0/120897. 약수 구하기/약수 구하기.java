d9e586ce1588ee01086e58c126bc174f98bd59b1
Hyunjin Park <132244618+hyun-Jin-Park-4808@users.noreply.github.com>
2026-01-05 22:16:22 +0900

    [level 0] Title: 약수 구하기, Time: 3.29 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n) {
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i = 1; i <= Math.sqrt(n); i++) {
+            if(n % i == 0) {
+                list.add(i);
+                if(i != n / i) {
+                    list.add(n / i);
+                }
+            }
+        }
+        return list.stream().sorted().mapToInt(Integer::intValue).toArray();
+    }
+}
